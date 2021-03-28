1f04cb3ea9e76c65f1b6c834adaeb8eb8ae99c31
aresfun-sandler <alex-sandler@mail.ru>
2021-03-28 13:11:54 +0300

    3. Интерфейс Input [#479622]

--- src/main/java/ru/job4j/tracker/Bus.java
+++ src/main/java/ru/job4j/tracker/Bus.java
@@ -4,16 +4,16 @@ public class Bus implements Transport {
 
     @Override
     public void ride() {
-
+        System.out.println(getClass().getSimpleName() + "ездит по трасам");
     }
 
     @Override
     public void passengers(int quantity) {
-
+        System.out.println(getClass().getSimpleName() + 20);
     }
 
     @Override
     public double refuel(double fuel) {
-        return fuel * 4.45;
+        return (fuel * 7) / 100;
     }
 }
