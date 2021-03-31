92a407dd6bbd56d1086a3be8d1bb6687dff540e5
aresfun-sandler <alex-sandler@mail.ru>
2021-03-31 15:07:42 +0300

    0.5. Иерархия исключений и множественный catch. [#479643]

---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,43 @@
+package ru.job4j.ex;
+
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        User rsl = null;
+        for (int i = 0; i < users.length; i++) {
+            String el = users[i].getUsername();
+            if (el.equals(login)) {
+                rsl = users[i];
+                break;
+            }
+        }
+        if (rsl == null) {
+            throw new UserNotFoundException("User is not found");
+        }
+        return rsl;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        boolean rsl = true;
+        if (!user.isValid() || user.getUsername().length() < 3) {
+            throw new UserInvalidException("Invalid user");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            User[] users = {
+                    new User("Petr Arsentev", true)
+            };
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException ea) {
+            ea.printStackTrace();
+        } catch (UserNotFoundException en) {
+            en.printStackTrace();
+        }
+    }
+}
