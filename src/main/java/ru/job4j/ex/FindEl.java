fa2f48c6066bf4f8265632f95d5023dd139271ca
aresfun-sandler <alex-sandler@mail.ru>
2021-03-31 12:04:00 +0300

    0.4. Пользовательские исключения. [#479642]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            String el = value[i];
+            if (el.equals(key)) {
+                rsl = i;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("The item was not found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+       try {
+           indexOf(new String[] {"Klara", "Ukrala", "Klarnet"}, "Klarnet");
+       } catch (ElementNotFoundException e) {
+           e.printStackTrace();
+       }
+    }
+}
