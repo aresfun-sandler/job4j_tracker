b086c41b89abec2c6d0e152ccf03beedc1de1a05
aresfun-sandler <alex-sandler@mail.ru>
2021-04-13 17:36:41 +0300

    1. Телефонный справочник на базе ArrayList [#479599]

--- src/test/java/ru.job4j/collection/PhoneDictionaryTest.java
+++ src/test/java/ru.job4j/collection/PhoneDictionaryTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.IsNull.nullValue;
 
 public class PhoneDictionaryTest {
     @Test
@@ -14,6 +15,16 @@ public class PhoneDictionaryTest {
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
         ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenNotFind() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("5555555");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
 }
