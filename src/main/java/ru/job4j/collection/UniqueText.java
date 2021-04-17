f6cc9b56fd6ab70851b35035dd6612fc75be94aa
aresfun-sandler <alex-sandler@mail.ru>
2021-04-18 01:40:49 +0300

    2. Идентичные тексты [#479613]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.lang.reflect.Array;
+import java.util.Collections;
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String org : origin) {
+            check.add(org);
+        }
+        for (String txt : text) {
+           rsl = check.contains(txt);
+        }
+        return rsl;
+    }
+}
