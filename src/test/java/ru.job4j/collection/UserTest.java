4535d86494f2c5c9488f8a154c0b5042790390b4
aresfunn <salvador_oryol@mail.ru>
2021-06-21 18:37:49 +0300

    1. Организовать сортировку User [#10034]

---
+++ src/test/java/ru.job4j/collection/UserTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.collection;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class UserTest extends TestCase {
+
+    @Test
+    public void testCompareTo() {
+        Set<User> users = new TreeSet<>();
+        users.add(new User("Petr", 32));
+        users.add(new User("Ivan", 31));
+        Iterator<User> it = users.iterator();
+        assertThat(it.next(), is(new User("Ivan", 31)));
+        assertThat(it.next(), is(new User("Petr", 32)));
+    }
+
+    @Test
+    public void testCompareToEqualNames() {
+        Set<User> users = new TreeSet<>();
+        users.add(new User("Petr", 32));
+        users.add(new User("Petr", 31));
+        Iterator<User> it = users.iterator();
+        assertThat(it.next(), is(new User("Petr", 31)));
+        assertThat(it.next(), is(new User("Petr", 32)));
+    }
+
+        @Test
+        public void whenComparePertVSIvan() {
+            int rsl = new User("Petr", 32)
+                    .compareTo(
+                            new User("Ivan", 31)
+                    );
+            assertThat(rsl, greaterThan(0));
+        }
+
+    @Test
+    public void whenCompareEqualName() {
+        int rsl = new User("Petr", 32)
+                .compareTo(
+                        new User("Petr", 31)
+                );
+        assertThat(rsl, greaterThan(0));
+    }
+}
