c4262461da7784441fcfa2b23389fadc7dc6ff2e
aresfunn <salvador_oryol@mail.ru>
2021-06-24 00:07:54 +0300

    3. Объединить два отсортированных массива

---
+++ src/main/java/MassiveSort.java
@@ -0,0 +1,15 @@
+public class MassiveSort {
+    public static int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+        int i = 0, j = 0, z = 0;
+        while (i + j < rsl.length) {
+            if (j == right.length || i != left.length
+                    && left[i] < right[j]) {
+                rsl[i + j] = left[i++];
+            } else {
+                rsl[i + j] = right[j++];
+            }
+        }
+        return rsl;
+    }
+}
