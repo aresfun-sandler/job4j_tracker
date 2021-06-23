a5fca624c28909f4cae46c1ef7e64fe33d708801
aresfunn <salvador_oryol@mail.ru>
2021-06-23 06:46:20 +0300

    4. Сортировка номера [#221284]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -8,10 +8,6 @@ public class LexSort implements Comparator<String> {
     public int compare(String left, String right) {
         String[] lft = left.split(" ");
         String[] rgt = right.split(" ");
-//        StringBuffer sb = new StringBuffer(lft[0]);
-//        StringBuffer sb2 = new StringBuffer(rgt[0]);
-//        sb.setCharAt(lft[0].length() - 1, ' ');
-//        sb2.setCharAt(rgt[0].length() - 1, ' ');
         for (int i = 0; i < lft.length; i++) {
             int rsl = Integer.compare(Integer.parseInt(lft[i]), Integer.parseInt(rgt[i]));
             if (rsl != 0) {
@@ -19,7 +15,5 @@ public class LexSort implements Comparator<String> {
             }
         }
         return Integer.compare(left.length(), right.length());
-//        return Integer.compare(Integer.parseInt(lft[0].substring(0, lft.length - 1)),
-//                Integer.parseInt(rgt[0].substring(0, rgt.length - 1)));
     }
 }
