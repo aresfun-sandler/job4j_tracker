7e15d194708e1f7736d74af0233a6a25eb63ecf4
aresfunn <salvador_oryol@mail.ru>
2021-07-04 16:40:03 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,10 +1,13 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+        Comparator<Address> cmp = Comparator.comparing(Address::getCity);
+        return profiles.stream().map(Profile::getAddress)
+                .sorted(cmp).distinct().collect(Collectors.toList());
     }
 }
--- src/test/java/ru.job4j/stream/ProfilesTest.java
+++ src/test/java/ru.job4j/stream/ProfilesTest.java
@@ -2,7 +2,11 @@ package ru.job4j.stream;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -12,17 +16,18 @@ public class ProfilesTest {
     public void testCollect() {
         List<Profile> list = List.of(
                 new Profile(new Address("Rostov", "Zorge", 12, 4)),
-                new Profile(new Address("Rostov", "Vetrogradova", 34, 3)),
-                new Profile(new Address("Rostov", "Stachky", 5, 1)),
-                new Profile(new Address("Rostov", "Malinovkogo", 13, 5))
+                new Profile(new Address("Moscow", "Vetrogradova", 34, 3)),
+                new Profile(new Address("Novgorod", "Stachky", 5, 1)),
+                new Profile(new Address("Lugansk", "Malinovkogo", 13, 5)),
+                new Profile(new Address("Rostov", "Zorge", 12, 4))
         );
         Profiles profiles = new Profiles();
         List<Address> rsl = profiles.collect(list);
         List<Address> expected = List.of(
-                new Address("Rostov", "Zorge", 12, 4),
-                new Address("Rostov", "Vetrogradova", 34, 3),
-                new Address("Rostov", "Stachky", 5, 1),
-                new Address("Rostov", "Malinovkogo", 13, 5)
+                new Address("Lugansk", "Malinovkogo", 13, 5),
+                new Address("Moscow", "Vetrogradova", 34, 3),
+                new Address("Novgorod", "Stachky", 5, 1),
+                new Address("Rostov", "Zorge", 12, 4)
         );
         assertThat(rsl, is(expected));
     }
