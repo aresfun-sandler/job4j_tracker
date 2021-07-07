08aac5b1a48292a14ad697fb168fc03b37dc40a1
aresfunn <salvador_oryol@mail.ru>
2021-07-07 20:14:57 +0300

    3. Банковские переводы [#10038]

--- src/main/java/ru/job4j/collection/BankService.java
+++ src/main/java/ru/job4j/collection/BankService.java
@@ -1,50 +1,57 @@
-//package ru.job4j.collection;
-//
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-//
-//public class BankService {
-//    private Map<User, List<AccountBank>> users = new HashMap<>();
-//
-//    public void addUser(User user) {
-//        if (!users.containsKey(user)) {
-//            users.put(user, new ArrayList<AccountBank>());
-//        }
-//    }
-//
-//    public void addAccount(String passport, AccountBank accountBank) {
-//        User user = findByPassport(passport);
-//        if (!users.containsKey(user)) {
-//            users.put(user, new ArrayList<AccountBank> accountBank);
-//        }
-//    }
-//
-//    public User findByPassport(String passport) {
-//        User user = null;
-//        for (User key : users.keySet()) {
-//            List<AccountBank> value = users.get(key);
-//            if (key.getPassport().contains(passport)) {
-//                user = key;
-//            }
-//        }
-//        return user;
-//    }
-//
-//        public AccountBank findByRequisite (String passport, String requisite) {
-//        AccountBank accountBank = null;
-//        User user = findByPassport(passport);
-//        if (user.contains(requisite)) {
-//            accountBank = r
-//        }
-//            return accountBank;
-//        }
-//
-//        public boolean transferMoney (String srcPassport, String srcRequisite,
-//                String destPassport, String destRequisite,double amount){
-//            boolean rsl = false;
-//            return rsl;
-//        }
-//    }
-//}
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class BankService {
+    private Map<User, List<AccountBank>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        if (!users.containsKey(user)) {
+            users.put(user, new ArrayList<AccountBank>());
+        }
+    }
+
+    public void addAccount(String passport, AccountBank accountBank) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<AccountBank> accountList = users.get(user);
+            if (!accountList.contains(accountBank)) {
+                accountList.add(accountBank);
+            }
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User key : users.keySet()) {
+            if (key.getPassport().equals(passport)) {
+                return key;
+            }
+        }
+        return null;
+    }
+
+    public AccountBank findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<AccountBank> accountList = users.get(user);
+            for (AccountBank list : accountList) {
+                if (list.getRequisite().equals(requisite)) {
+                    return list;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        AccountBank srcAccount = findByRequisite(srcPassport, srcRequisite);
+        AccountBank destAccount = findByRequisite(destPassport, destRequisite);
+        if (srcAccount != null && destAccount != null && srcAccount.getBalance() >= amount) {
+            srcAccount.setBalance(srcAccount.getBalance() - amount);
+            destAccount.setBalance(destAccount.getBalance() + amount);
+            return true;
+        }
+        return false;
+    }
+}
