4529c763cc8f10eff620dc943838e0f8fc117965
aresfunn <salvador_oryol@mail.ru>
2021-07-12 01:40:07 +0300

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/collection/BankService.java
+++ src/main/java/ru/job4j/collection/BankService.java
@@ -2,13 +2,32 @@ package ru.job4j.collection;
 
 import java.util.*;
 
+/**
+ * Класс описывает банковскую систему
+ * @author ALEX SANDLER
+ * @version 1.0
+ */
 public class BankService {
+
+    /**
+     * Пользователи системы с привязанными к ним счетами хранятся в колекции типа HashMap
+     */
     private Map<User, List<AccountBank>> users = new HashMap<>();
 
+    /**
+     * Метод добавляет пользователя в систему
+     * @param user новый пользователь
+     */
     public void addUser(User user) {
             users.putIfAbsent(user, new ArrayList<>());
     }
 
+    /**
+     * Метод добавляет пользователю новый аккаунт, предварительно проверяя
+     * отсутствие данного аккаунта в системе: если совпадений не найдено - добавление.
+     * @param passport паспорт пользователя
+     * @param accountBank аккаунт пользователя
+     */
     public void addAccount(String passport, AccountBank accountBank) {
         Optional<User> user = findByPassport(passport);
         if (user.isPresent()) {
@@ -19,6 +38,11 @@ public class BankService {
         }
     }
 
+    /**
+     * Метод ищет пользователя по номеру паспорта
+     * @param passport паспорт пользователя
+     * @return возвращает пользователя или null, если пользователь не найден.
+     */
     public Optional<User> findByPassport(String passport) {
            return users.keySet().stream()
                    .filter(s -> s.getPassport()
@@ -26,6 +50,12 @@ public class BankService {
                    .findFirst();
     }
 
+    /**
+     * Метод ищет счет пользователя по реквизитам
+     * @param passport  паспорт пользователя.
+     * @param requisite реквизиты пользователя.
+     * @return возвращает счёт пользователя или null, если счёт не найден.
+     */
     public AccountBank findByRequisite(String passport, String requisite) {
         Optional<User> user = findByPassport(passport);
         return user.flatMap(value -> users.get(value).stream()
@@ -33,6 +63,15 @@ public class BankService {
                 .findFirst()).orElse(null);
     }
 
+    /**
+     * метод для перечисления денег с одного счета, на другой.
+     * @param srcPassport   паспорт пользователя, откуда будет производится перечисление
+     * @param srcRequisite  реквизиты пользователя, откуда будет производится перечисление
+     * @param destPassport  паспорт пользователя, куда будет производится перечисление
+     * @param destRequisite реквизиты пользователя, куда будет производится перечисление
+     * @param amount        сумма перечисления.
+     * @return возвращает успешность перевода
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         AccountBank srcAccount = findByRequisite(srcPassport, srcRequisite);
