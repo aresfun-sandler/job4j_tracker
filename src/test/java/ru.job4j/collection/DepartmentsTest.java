ee378102efb47cc047379fde95740b068ec94fb3
aresfunn <salvador_oryol@mail.ru>
2021-08-15 16:04:18 +0300

    2. Отсортировать департаменты [#34352]

--- src/test/java/ru.job4j/collection/DepartmentsTest.java
+++ src/test/java/ru.job4j/collection/DepartmentsTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.collection;
 
 import org.junit.Test;
+
+import java.util.Arrays;
 import java.util.List;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -20,5 +22,25 @@ public class DepartmentsTest {
         List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSortAsc() {
+        List<String> input = Arrays.asList("K1/SK1/SSK1", "K2/SK1/SSK1",
+                "K1/SK1/SSK2", "K1/SK2", "K1/SK1", "K2/SK1/SSK2");
+        List<String> expect = List.of("K1", "K1/SK1", "K1/SK1/SSK1", "K1/SK1/SSK2",
+                "K1/SK2", "K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2");
+        Departments.sortAsc(input);
+        assertThat(input, is(expect));
+    }
+
+    @Test
+    public void whenSortDesc() {
+        List<String> input = Arrays.asList("K1/SK1/SSK1", "K2/SK1/SSK1",
+                "K1/SK1/SSK2", "K1/SK2", "K1/SK1", "K2/SK1/SSK2");
+        List<String> expect = List.of("K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2", "K1",
+                "K1/SK1", "K1/SK1/SSK1", "K1/SK1/SSK2", "K1/SK2");
+        Departments.sortDesc(input);
+        assertThat(input, is(expect));
     }
 }
